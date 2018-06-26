8e639a6721907fe34d9b54d05304a946421d01ee
Lynn Kirby <lynn@arrak.is>
2018-06-26 07:06:40 -0400

    fix(termcolor): actually compile under MSVC (#2)

--- examples/termcolor.c
+++ examples/termcolor.c
@@ -1,9 +1,15 @@
 #ifdef _WIN32
+
 #define WINVER 0x0600
 #define _WIN32_WINNT 0x0600
 #define NTDDI_VERSION 0x06000000
+#define VC_EXTRALEAN
+#define WIN32_LEAN_AND_MEAN
+
 #else
+
 #define _POSIX_C_SOURCE 1
+
 #endif
 
 #define LPD_TERMCOLOR_IMPLEMENTATION
