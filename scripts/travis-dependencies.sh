2a128aa056312d1060a1a514b688df8e2eb4e11b
Lynn Kirby <lynn@arrak.is>
2018-06-28 03:50:14 -0400

    amazing dbugging

--- scripts/travis-dependencies.sh
+++ scripts/travis-dependencies.sh
@@ -8,8 +8,9 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 else
     sudo apt-get update -q
     sudo apt-get install -q ninja-build python-pip
-    pip install -U pip
     pip install pipenv --user
     pipenv install
-    pipenv run python -c "import pkg_resources; print(list(pkg_resources.iter_entry_points('bfg9000.backends')))"
+    pipenv run python -c "import pkg_resources; print(list(pkg_resources.iter_entry_points('bfg9000.backends'))[0].load())"
+    pipenv run python -c "import pkg_resources; print(list(pkg_resources.iter_entry_points('bfg9000.backends'))[1].load())"
+    pipenv run python -c "import pkg_resources; print(list(pkg_resources.iter_entry_points('bfg9000.backends'))[2].load())"
 fi
