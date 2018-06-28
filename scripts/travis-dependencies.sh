ba553967c60cf90a825b9473926af93de0d88e96
Lynn Kirby <lynn@arrak.is>
2018-06-28 04:03:09 -0400

    damn patchelf

--- scripts/travis-dependencies.sh
+++ scripts/travis-dependencies.sh
@@ -9,8 +9,6 @@ else
     sudo apt-get update -q
     sudo apt-get install -q ninja-build python-pip
     pip install pipenv --user
-    pipenv install
-    pipenv run python -c "import pkg_resources; print(list(pkg_resources.iter_entry_points('bfg9000.backends'))[0].load())"
-    pipenv run python -c "import pkg_resources; print(list(pkg_resources.iter_entry_points('bfg9000.backends'))[1].load())"
-    pipenv run python -c "import pkg_resources; print(list(pkg_resources.iter_entry_points('bfg9000.backends'))[2].load())"
+    pipenv graph
+    NOPATCHELF=1 pipenv install
 fi
