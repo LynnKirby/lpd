fb78f5ae25af2f3de18103bb6a59d0879ec0995b
Lynn Kirby <lynn@arrak.is>
2018-06-28 01:57:53 -0400

    fix travis

--- scripts/travis-dependencies.sh
+++ scripts/travis-dependencies.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install ninja pipenv
+    HOMEBREW_NO_AUTO_UPDATE=1
+    brew update
+    brew install ninja pipenv
     pipenv install
 else
     sudo apt-get update -q
     sudo apt-get install -q ninja-build python-pip
-    pip install pipenv
+    sudo pip install pipenv
     pipenv install
 fi
